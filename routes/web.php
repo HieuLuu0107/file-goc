d295d70fe065fdf0b1ff10a66d5254cdc28758b6
Trọng Hiếu <hieuluucucxuc@gmail.com>
2025-04-08 20:56:13 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome'); // hoặc return 'Laravel đang chạy!';
-});
-
-Route::get('/test', function () {
-    return 'Đây là trang test';
+    return view('welcome');
 });
